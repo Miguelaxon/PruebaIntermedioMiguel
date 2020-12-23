f9d9ad3b8ad4d8603dae6b75a84d134d798ecaf9
miguel <miguel_ahumada_c@hotmail.com>
2020-12-23 13:58:45 -0300

    se crean las vistas y se configuran unas redes sociales, además de configurar un envío de correo

--- app/src/main/java/com/example/pruebaintermediomiguel/ContactActivity.java
+++ app/src/main/java/com/example/pruebaintermediomiguel/ContactActivity.java
@@ -2,13 +2,53 @@ package com.example.pruebaintermediomiguel;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.example.pruebaintermediomiguel.databinding.ActivityContactBinding;
 
 public class ContactActivity extends AppCompatActivity {
-
+    private ActivityContactBinding eBinding;
+    Intent linkIntent = new Intent(Intent.ACTION_VIEW);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_contact);
+        eBinding = ActivityContactBinding.inflate(getLayoutInflater());
+        setContentView(eBinding.getRoot());
+
+        eBinding.imageViewLin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linkIntent.setData(Uri.parse("http://www.linkedin.com/in/miguelantonioahumadacaamaño"));
+                startActivity(linkIntent);
+            }
+        });
+
+        eBinding.imageViewFace.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                linkIntent.setData(Uri.parse("https://www.facebook.com/Miguel.El.Bulla/"));
+                startActivity(linkIntent);
+            }
+        });
+
+        eBinding.imageViewPhone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                shareWhatsApp("Hola, gracias por contactar, \ndeje su mensaje para contactarnos.");
+            }
+        });
+    }
+    public void shareWhatsApp(String mensaje){
+        linkIntent = new Intent(Intent.ACTION_SEND);
+        linkIntent.putExtra(Intent.EXTRA_TEXT, mensaje);
+        linkIntent.setType("text/plain");
+        linkIntent.setPackage("com.whatsapp");
+        //linkIntent.setPackage("https://wa.me/56951316411");
+        //linkIntent.setPackage("https://api.whatsapp.com/send?phone=56951316411");
+        startActivity(linkIntent);
     }
 }
--- app/src/main/java/com/example/pruebaintermediomiguel/MainActivity.java
+++ app/src/main/java/com/example/pruebaintermediomiguel/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.pruebaintermediomiguel;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.bumptech.glide.Glide;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -12,6 +14,7 @@ import androidx.appcompat.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -24,12 +27,17 @@ public class MainActivity extends AppCompatActivity {
         CollapsingToolbarLayout toolBarLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
         toolBarLayout.setTitle(getTitle());
 
+        ImageView imageView1 = findViewById(R.id.imageViewAM);
+        String url01 = "https://media-exp1.licdn.com/dms/image/C4E35AQEQjpSm2FGiJQ/profile-framedphoto-shrink_200_200/0/1606652051962?e=1608818400&v=beta&t=uwN_th0CekLpu8ymd1xsh_DDce1VpKwE5wENQhLSWW8";
+        String url = "https://lh3.googleusercontent.com/9Gv1dCcuqkKXL3xwSBM7oxqtEA8pEUL465DwCm_euDqeoGz1VsPcno-1gzqhRAQNkZQz0CxvY2FDpUORo1K-SOVc63rdFprbRCReIHPwue--UIrhCYI71Jyp9XrGuGzTfJyAgie6Y4gc8n_xSWmNcT33L1KCxo-MTllJSBNP39D4RWiL_DMcGIK-reKRRnVte9dPK2wX9-yoAgaSI467dh8HIJNBSP1fGs-KC9WGa--HOe5Pcb1Qmq8cJ6_EFSHLHisXeJ6L7aDxJsR10s8Sxgu3RQQMtXQxhiW2U7mYaybo7rQ8GDI-rMS4h5wHtSj8MlN7UfalppuwqNkvUTBWA-GCOKtP-o3HsHRSkGMuYrnD6Zut2iFtKBe3WySAZ7accJuAfSiDGD7yG8k5yq5ff5TYIcnnSb8zGQuLsSNgVopan9ifoFNJwrGv5yqBJRKc_62g8x8euadUXhK-mJ-IvOo8gCbRkKYFQM-fhbeYUXqzXmr6maZ1ipoaFFnH0Vs803Lp2Ateis3Go73Y_6tPWMbCUUY79fn5h7Y0byGqh78981lvr3oKyXiNvyznO9PIbKD8jFEix50QfSoJXRCWsMm5PPvz_exPe7uU-iNerR540KQdzf6cSaVt9618cqjhFWSAWl7QuM9hJ6BzXJHp2mfGO3d7UIsN5yCrCEr1zlqepFOvb_tYoK7s-e4FeA=w834-h625-no?authuser=0";
+        Glide.with(this).load(url01).centerCrop().into(imageView1);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(MainActivity.this,ContactActivity.class);
+                startActivity(intent);
             }
         });
     }
